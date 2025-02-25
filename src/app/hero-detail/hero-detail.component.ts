336c2d9425cd6dc71ac7be7aaffaf8af90a5fce3
ric22otta <riccardo.buonocore08@gmail.com>
2025-02-25 19:45:28 +0100

    Task 3: Create a feature component

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import {Hero} from '../hero';
 
 @Component({
   selector: 'app-hero-detail',
@@ -7,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './hero-detail.component.css'
 })
 export class HeroDetailComponent {
-
+  @Input() hero?: Hero;
 }
