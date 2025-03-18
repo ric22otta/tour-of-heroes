cc6c498a337b3910b8f4fda95764a4bf54b87f28
ric22otta <riccardo.buonocore08@gmail.com>
2025-03-18 20:30:29 +0100

    🐛 fix: Randomizer

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -19,6 +19,21 @@ export class DashboardComponent {
 
   getHeroes(): void {
     this.heroService.getHeroes()
-      .subscribe(heroes => this.heroes = heroes.slice(1, 5));
+      .subscribe(heroes => {
+        let n: number[] = [];
+        for (let i: number = 0; i < 4 && i < heroes.length; i++) {
+          let r: number = Math.floor(Math.random() * heroes.length);
+          for (let j: number = 0; j < i; j++) {
+            if (r === n[j]) {
+              j = -1;
+              r = Math.floor(Math.random() * heroes.length);
+            }
+          }
+          n.push(r);
+        }
+        for (let i: number = 0; i < n.length; i++) {
+          this.heroes.push(heroes[n[i]]);
+        }
+      });
   }
 }
